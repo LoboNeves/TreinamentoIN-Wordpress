8f37995bcfee9c2abd3372066cbc98fd16960932
Daniel Neves <dbenditoneves@gmail.com>
2020-08-11 21:45:39 -0300

    Tarefa do segundo dia realizada

--- wp-content/themes/brafe/page-contato.php
+++ wp-content/themes/brafe/page-contato.php
@@ -4,4 +4,5 @@
 
 <?php get_header(); ?>
     <h1>Contato</h1>
+    <h2><?php the_field('texto_teste') ?></h2>
 <?php get_footer(); ?>
--- wp-content/themes/brafe/page-home.php
+++ wp-content/themes/brafe/page-home.php
@@ -6,24 +6,21 @@
 <section id="sct1-hmpg">
         <div class="container">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bg-intro.jpg" alt="Wallpaper de cafés" style="width:100%">
-            <div class="centered"><h1>CAFÉS COM A CARA DO BRASIL</h1></div>
-            <div class="centered"><h2>Direto das fazendas de Minas Gerais</h2></div>
+            <div class="centered"><h1><?php the_field('titulo_chamativo') ?></h1></div>
+            <div class="centered"><h2><?php the_field('sub-titulo_chamativo') ?></h2></div>
             <div class="centered"><p>-</p></div>
         </div>
     </section>
     <section id="sct2-hmpg">
         <div class="container">
-            <h1><b>Uma mistura de</b></h1>
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cafe-1.jpg" alt="Foto de café">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cafe-2.jpg" alt="Foto de café">
+            <h1><b><?php the_field('texto_em_cima_de_cafes') ?></b></h1>
+            <img src="<?php the_field('imagem_cafe1') ?>" alt="Foto de café">
+            <img src="<?php the_field('imagem_cafe2') ?>" alt="Foto de café">
             <div class="captions">
                 <figcaption>amor</figcaption>
                 <figcaption>perfeição</figcaption>
             </div>
-            <h2>O café é uma bebida produzida a partir dos grãos<br>torrados do fruto do cafeeiro. É servido
-                tradicionalmente<br> quente, mas também pode ser consumido gelado.<br> Ele é um estimulante,
-                por possuir cafeína — geralmente<br> 80 a 140 mg para cada 207 ml dependendo do método<br> de
-                preparação.</h2>
+            <h2><?php the_field('texto_em_baixo_de_cafes') ?></h2>
         </div>
     </section>
     <section id="sct3-hmpg">
@@ -33,51 +30,45 @@
             <div class="circle"></div>
         </div>
         <div class="Nomes">
-            <h1>Paulista</h1>
-            <h1>Carioca</h1>
-            <h1>Mineiro</h1>
+            <h1><?php the_field('titulo_paulista') ?></h1>
+            <h1><?php the_field('titulo_carioca') ?></h1>
+            <h1><?php the_field('titulo_mineiro') ?></h1>
         </div>
         <div class="circles subs">
-            <p>As condições climáticas não eram as melhores<br>nessa primeira escolha e, entre 1800 e 1850<br>,
-            tentou-se o cultivo noutras regiões: o João<br> Alberto Castelo Branco trouxe mudas do Pará</p>
-            <p>As condições climáticas não eram as melhores<br>nessa primeira escolha e, entre 1800 e 1850<br>,
-            tentou-se o cultivo noutras regiões: o João<br> Alberto Castelo Branco trouxe mudas do Pará</p>
-            <p>As condições climáticas não eram as melhores<br>nessa primeira escolha e, entre 1800 e 1850<br>,
-            tentou-se o cultivo noutras regiões: o João<br> Alberto Castelo Branco trouxe mudas do Pará</p>
+            <p><?php the_field('texto_paulista') ?></p>
+            <p><?php the_field('texto_carioca') ?></p>
+            <p><?php the_field('texto_mineiro') ?></p>
         </div>
         <h3>SAIBA MAIS</h3>
     </section>
     <section id="sct4-hmpg">
         <div class="card">
             <div>
-                <figure><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/botafogo.jpg" alt="Botafogo"></figure>
+                <figure><img src="<?php the_field('imagem_botafogo') ?>" alt="Botafogo"></figure>
             </div>
             <div>
-            <h3>Botafogo</h3>
-            <p>As condições climáticas não eram as<br>melhores nessa primeira escolha e, entre<br> 1800 e 1850,
-                tentou-se o cultivo.</p>
+            <h3><?php the_field('titulo_botafogo') ?></h3>
+            <p><?php the_field('texto_botafogo') ?></p>
             <h4>VER MAPA</h4>
             </div>
         </div>
         <div class="card">
             <div>
-                <figure><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/iguatemi.jpg" alt="Iguatemi"></figure>
+                <figure><img src="<?php the_field('imagem_iguatemi') ?>" alt="Iguatemi"></figure>
             </div>
             <div>
-            <h3>Iguatemi</h3>
-            <p>As condições climáticas não eram as<br>melhores nessa primeira escolha e, entre<br> 1800 e 1850,
-                tentou-se o cultivo.</p>
+            <h3><?php the_field('titulo_iguatemi') ?></h3>
+            <p><?php the_field('texto_iguatemi') ?></p>
             <h4>VER MAPA</h4>
             </div>
         </div>
         <div class="card">
             <div>
-                <figure><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mineirao.jpg" alt="Botafogo"></figure>
+                <figure><img src="<?php the_field('imagem_mineirao') ?>" alt="Mineirão"></figure>
             </div>
             <div>
-            <h3>Mineirão</h3>
-            <p>As condições climáticas não eram as<br>melhores nessa primeira escolha e, entre<br> 1800 e 1850,
-                tentou-se o cultivo.</p>
+            <h3><?php the_field('titulo_mineirao') ?></h3>
+            <p><?php the_field('texto_mineirao') ?></p>
             <h4>VER MAPA</h4>
             </div>
         </div>
